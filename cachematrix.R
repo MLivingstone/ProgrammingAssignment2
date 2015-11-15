38b77e9d1950d0fa590b530b18c15f7109d0c282
MLivingstone <martlivingstone@gmail.com>
2015-11-15 10:07:45 +0300

    Assignment submission

--- cachematrix.R
+++ cachematrix.R
@@ -3,9 +3,7 @@
 # repeatedly. The following two functions are used to cache the inverse
 # of a matrix
 
-
 ## makeCacheMatrix creates a list containing a function to:
-
 # set the value of the matrix
 # get the value of the matrix
 # set the value of the inverse of the matrix
@@ -20,13 +18,14 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function() x
         setinv <- function(minv) inv <<- minv
         getinv <- function() inv
+        
         list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
 }
 
 ## cacheSolve returns the inverse of a matrix.
-## It assumes that the matrix is square and invertible.
+# It assumes that the matrix is square and invertible.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
